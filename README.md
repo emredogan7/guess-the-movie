b237cafc8753ff30cf5f26537b3d1f174db3b864
emredogan7 <mrdogan7@gmail.com>
2019-07-09 03:24:46 +0300

    Added README and updated the code.

---
+++ README.md
@@ -0,0 +1,7 @@
+# Guess the Moive, a simple command line game.
+- The purpose is to guess the movie name given the number of letters in it (pretty much like hangman but with movies).
+- A project from the Udacity course _Object Oriented Programming in Java_.
+
+
+
+                    
--- src/GuessTheMovie.java
+++ src/GuessTheMovie.java
@@ -1,24 +1,1 @@
-import java.util.*;
-import java.io.*;
-
-public class GuessTheMovie {
-
-    public static void main(String[] args) throws FileNotFoundException {
-        // creating the arraylist of movie names.
-        File file = new File("movies.txt");
-        Scanner sc = new Scanner(file);
-        int totLength = 0;
-        ArrayList<String> movieList = new ArrayList<String>();
-
-        while (sc.hasNextLine()) {
-            movieList.add(sc.nextLine());
-        }
-
-        System.out.println(movieList.size());
-
-        // selecting the random movie.
-        int randInd = (int)((Math.random() * (movieList.size()+1)));
-        String chosenMovie = movieList.get(randInd);
-
-    }
-}
+import java.util.*;import java.io.*;public class GuessTheMovie {    public static void main(String[] args) throws FileNotFoundException {        // creating the arraylist of movie names.        File file = new File("movies.txt");        Scanner sc = new Scanner(file);        int totLength = 0;        ArrayList<String> movieList = new ArrayList<String>();        while (sc.hasNextLine()) {            movieList.add(sc.nextLine());        }        // selecting the random movie.        int randInd = (int)((Math.random() * movieList.size()));        String chosenMovie = movieList.get(randInd);        String holderMovie = chosenMovie.replaceAll("[a-zA-Z]", "_");        System.out.println(holderMovie);        System.out.println(chosenMovie);        int wrongAnswers = 0;        Scanner in = new Scanner(System.in);        while (wrongAnswers < 10){            System.out.println("Your are guessing: " + holderMovie);            System.out.println("You have guessed ( " + wrongAnswers + " ) wrong letters:");            System.out.println("Please guess a letter: ");            char letter = in.nextLine().toCharArray()[0];            char[] holderMovieChar = holderMovie.toCharArray();            //System.out.println(holderMovie.replaceAll("_",letter));            int index = chosenMovie.indexOf(letter);            if (index == -1){                wrongAnswers++;                continue;            }            while (index >= 0) {                holderMovieChar[index] = letter;                index = chosenMovie.indexOf(letter, index + 1);            }            holderMovie = String.valueOf(holderMovieChar);            if(holderMovie.indexOf('_') == -1){                System.out.println("Well done. You win the game. The movie is " + chosenMovie);                break;            }        }        if(wrongAnswers == 10){        System.out.println("Sorry. U lost the game.");        System.out.println("The movie was " + chosenMovie);        }    }}
